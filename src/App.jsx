54f51b943c6cbafd37195fba5a4b7767335b2bc7
MaxMariani <wmaxmariani@gmail.com>
2025-04-05 14:27:35 +0200

    Collecting User Input with Refs & Forwarded Refs

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,33 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    const newProject = {
+      ...projectData,
+      id: Math.random,
+    };
+
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject}/>;
   } else if (projectsState.selectedProjectId === undefined) {
-    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
       {content}
     </main>
   );
